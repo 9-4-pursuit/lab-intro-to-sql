b0043c1de3aa176d0d74d090b3b268d6f274f6a3
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-01 01:09:40 -0400

    showing all data in gifts table

--- regifter.sql
+++ regifter.sql
@@ -31,17 +31,17 @@ previously_regifted BOOLEAN DEFAULT false);
 -- 
 \echo Alter the table so that the column price is changed to value 
 -- 
-ALTER TABLE gifts RENAME price TO value ;
+ALTER TABLE gifts RENAME price to value ;
 
 -- 
 \echo Insert a peach candle, given by 'Santa' thats value is 9 and has been previously regifted
 -- 
-
+INSERT INTO gifts (gift, giver, value, previously_regifted) VALUES ('peach candle', 'Santa', 9, true);
 
 --
 \echo Query for all the columns in your gifts table
 -- 
-
+SELECT * FROM gifts ;
 
 --
 \echo Uncomment below to insert 5 more gifts
