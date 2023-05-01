e368225e77b84a9413148125e5afb2761ec8830f
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-01 01:36:17 -0400

    query for all gifts contaning 'candle'

--- regifter.sql
+++ regifter.sql
@@ -79,7 +79,7 @@ SELECT * FROM gifts WHERE value >= 20 ;
 \echo Query for every gift that has the word candle in it, only show the gift column
 --
 
-
+SELECT * FROM gifts WHERE gift ILIKE '%candle%';
 --
 \echo Query for every gift whose giver is Santa OR value is greater than 30
 --
